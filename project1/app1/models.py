3c560f41dec199342ac92836e43e4cab24b819c6
Meimoune Ahmeide <maxhmd5050@gmail.com>
2023-11-14 14:51:01 +0100

    enleve hello

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,10 +1,12 @@
-from unicodedata import name
+
 from django.db import models
 
 class products(models.Model):
     name = models.CharField(max_length=150)
     description=models.TextField()
-    price=models.DecimalField(max_digits=None, decimal_places=5)
+    price=models.DecimalField(max_digits=10000, decimal_places=4
+    )
+
 
 """
 chaque fois je modifie ma table ou ma classe du model je besion de taper :
--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-print("hello")
 def home(request):
     return render(request, "index.html")
