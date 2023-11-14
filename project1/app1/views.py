3fc2226e47ab5c30c65c49fb0f10eb298977e1de
Meimoune Ahmeide <maxhmd5050@gmail.com>
2023-11-14 15:18:05 +0100

    quelle que modifications

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
-=======
-print("hello")
-print("test")
->>>>>>> c5ab3fc12da9dd21ef540a5fbd36e7de499b0785
+
 def home(request):
     return render(request, "index.html")
+print("git OSAJSDIOADJIau")
